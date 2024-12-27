0e0b86bd68390af2b4e0ac3e3c9357400244eda1
bahadzhalalov <bahadzhalalov@gmail.com>
2024-12-27 21:11:21 +0500

    add migrations

---
+++ database/migrations/2024_12_27_155935_create_batch_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batch_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('batch_id')->constrained('batches')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->decimal('purchase_price', 10)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batch_products');
+    }
+};
