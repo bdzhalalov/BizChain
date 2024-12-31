04c2627d5f3ede9ab700853914747a6a66075a77
bahadzhalalov <bahadzhalalov@gmail.com>
2024-12-31 16:55:59 +0500

    add tests for creating order

--- tests/ApiTestCase.php
+++ tests/ApiTestCase.php
@@ -4,6 +4,7 @@ namespace Tests;
 
 use App\Models\Batch;
 use App\Models\Category;
+use App\Models\Client;
 use App\Models\Product;
 use App\Models\Provider;
 use App\Models\Storage;
@@ -26,6 +27,7 @@ class ApiTestCase extends TestCase
         $category = Category::factory()->withProvider($provider)->create();
         $batch = Batch::factory()->withProvider($provider)->create();
         $product = Product::factory()->withCategory($category)->create();
+        Client::factory()->create();
 
         $this->productQuantity = mt_rand(2, 50);
 
